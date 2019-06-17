b05a9c3c88db10f85ba1ccf48660057fb36cf642
Abdul Azeez <abdaseez@gmail.com>
2019-06-17 16:52:59 +0530

    ui done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
   <h1 class="display-3">Send Bulk SMS</h1>
   <p class="lead">Import excel sheet with Phone Numbers column, and send custom sms to all that numbers in just one click</p>
   <hr class="my-4"/>
+  <div class="form-group">
+      <label for="exampleInputFile">Import Excel</label>
+      <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp"></input>
+    </div>
+    <div class="form-group">
+      <label for="exampleTextarea">Type Custom message</label>
+      <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Send</button>
 </div>
   );
 }
